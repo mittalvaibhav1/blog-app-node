ffc4a96baff5581b7ea1a9714be087ba451ac6f0
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-03-09 15:33:30 +0530

    os module

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-console.log("!--------------------------------!");
-const {users, groups} = require('./modules/users');
-console.log("!--------------------------------!");
-const { themes } = require('./modules/themes');
-console.log("Themese", themes) // RETURNING MODIFIED ARRAY!!!!!!!!!!!!!!
-console.log("!--------------------------------!");
-
-// The require statment caches its result!!! so initalization code will run only once!! 
-// That is why themes is printed only once! because it's initalization code ran only once! 
+const os = require('os');
+console.log(os.version());
