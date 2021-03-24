63a61c2e0c4696f73f06a13e5370fc82f794848c
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-03-24 15:30:20 +0530

    fs writeStream

--- streams.js
+++ streams.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
 // ReadStream is used to read data.
+// WriteStream is used to write data. // creates the file if it doesn't exist or overwrites previous data
 
 const readStream = fs.createReadStream('./files/largeData.txt', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream('./files/output.txt');
+
 readStream.on('data', (chunk) => {
-    console.log('----- NEW CHUNK -----');
-    console.log(chunk);
+    writeStream.write(chunk);
 })
