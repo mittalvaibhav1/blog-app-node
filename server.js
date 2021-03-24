bd55af727489026656c733c811574b761cf53bdb
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-03-25 01:08:32 +0530

    creating server and listening to requests.

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+// To create a server!
+// http.createServer(callback(request, response))
+// This callback runs everytime you make a request to the server.
+const server = http.createServer((request, response) => {
+    console.log("Request has been made!");
+});
+
+// To make our server listen to requests.
+// server.listen(portnumber, hostname, callback)
+server.listen(3000, "localhost", () => {
+    console.log("Listening for requests on port 3000.");
+})
