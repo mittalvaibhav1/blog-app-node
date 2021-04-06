17f139d10e8eedb37f1ae6e59f703ce6e8976069
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-06 18:24:12 +0530

    req and res object

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const http = require("http");
 
-// To create a server!
-// http.createServer(callback(request, response))
-// This callback runs everytime you make a request to the server.
-const server = http.createServer((request, response) => {
-    console.log(request.url);
-    console.log(request.method);
+const server = http.createServer((req, res) => {
+    console.log("Request Made", req.method, req.url);
+    res.write("Hello Ji");
+    res.end("Whaaaaaaaaat");
 });
 
-// To make our server listen to requests.
-// server.listen(portnumber, hostname, callback)
 server.listen(3000, "localhost", () => {
-    console.log("Listening for requests on port 3000.");
+    console.log("This server is listening on port 3000...");
 })
