e24ecbab28ba9d6cca16d59a803c01e5af79ada9
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-09 00:25:20 +0530

    Response Obj

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log("Request Made", req.method, req.url);
+    res.setHeader("Content-Type", "text/plain");
     res.write("Hello Ji");
-    res.end("Whaaaaaaaaat");
+    res.end();
 });
 
 server.listen(3000, "localhost", () => {
