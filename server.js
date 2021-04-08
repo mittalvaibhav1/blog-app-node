93f04369143b24e6d6997147db9084dcea76115d
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-09 00:46:11 +0530

    sending back html file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log("Request Made", req.method, req.url);
     res.setHeader("Content-Type", "text/html");
-    res.write("<p>Paraaa</p>");
-    res.end();
+    fs.readFile("./views/index.html", (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    } )
 });
 
 server.listen(3000, "localhost", () => {
