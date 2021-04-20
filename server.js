556ba47caf3cdac15656537860d914fa3561efb2
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-20 22:37:08 +0530

    Mongoose

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // Create express app
 const app = express();
 
+// Connect to mongodb
+const dbURI = "mongodb+srv://admin:test1234@blogsite.gcfxl.mongodb.net/blogsite?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => {
+    console.log("Connected to db");
+    app.listen(3000, "localhost", () => {
+        console.log("Listening to requests on port 3000...");
+    });
+})
+.catch((err) => {
+    console.log(err)
+});
+
+
 // Register view engine
 app.set("view engine", "ejs");
-
-// app.set("views", "new name"); To change folder name for views
-
-// Listen for requests
-const server = app.listen(3000, "localhost", () => {
-    console.log("Listening to requests on port 3000...");
-});
 
 // Logger Middleware 
 
