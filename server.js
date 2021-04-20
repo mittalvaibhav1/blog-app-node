9b1a608539550fe61671bd2b9560d3c760d38da5
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-20 18:42:39 +0530

    custom middleware

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const express = require("express");
 // Create express app
 const app = express();
 
-// register view engine
-
+// Register view engine
 app.set("view engine", "ejs");
+
 // app.set("views", "new name"); To change folder name for views
 
 // Listen for requests
 const server = app.listen(3000, "localhost", () => {
     console.log("Listening to requests on port 3000...");
+});
+
+// Logger Middleware 
+
+app.use((req, res, next) => {
+    console.log("New request made: ");
+    console.log("host: ", req.hostname);
+    console.log("path", req.path);
+    console.log("method: ", req.method);
+    next(); // to move to the next block
 });
 
 app.get("/", (req, res) => {
