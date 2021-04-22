70cca6cde74e8881cdc422105b036a3150c341d7
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-04-23 00:18:28 +0530

    Express MVC

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,53 +1,12 @@
 const express = require("express");
-const Blog = require("../models/blog");
+const blogController = require("../controllers/blogController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    Blog.find().sort({ createdAt: - 1})
-    .then((result) => {
-        res.render("index", { title: "Home", blogs: result });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-router.post("/", (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-    .then((result) => {
-        res.redirect("/blogs");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-router.get("/create", (req, res) => {
-    res.render("create", { title: "Create"});
-});
-
-router.get("/:id", (req, res) => {
-    Blog.findById(req.params.id)
-    .then((result) => {
-        res.render("blog", { title: "Blog Details", blog: result });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-router.delete("/:id", (req, res) => {
-    Blog.findByIdAndDelete(req.params.id)
-    .then(result => {
-        res.json({
-            redirect: "/blogs"
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
+router.get("/", blogController.blog_index);
+router.post("/", blogController.blog_create_post);
+router.get("/create", blogController.blog_create_get);
+router.get("/:id", blogController.blog_details);
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
